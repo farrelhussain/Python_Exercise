8660bba82c34dd6af2349219083ac80dab991169
farrelhussain <farrelstar223@gmail.com>
2025-07-18 10:23:43 +0700

    Latihan 1

---
+++ Operator.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+
+penjumlahan= a+b
+pengurangan= a-b
+perkalian= a*b
+pembagian= a/b
+modulus= a%b
+
+print(penjumlahan, pengurangan,perkalian,pembagian,modulus)
---
+++ TipeData.py
@@ -0,0 +1,9 @@
+nilai_integer= 10
+nilai_float= 10.5
+nilai_string="Hallo Pyton"
+nilai_boolean= True
+
+print(int(nilai_integer))
+print(float(nilai_float))
+print(str(nilai_string))
+print(bool(nilai_boolean))
